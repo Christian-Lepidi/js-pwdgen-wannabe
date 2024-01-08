ea447e1cd9d9d6622288409af7066db401a1cb18
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-08 15:03:43 +0100

    feat: add. html text and password

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-const name = prompt("Qual è il tuo nome?");
-console.log(name);
+const completeElement = document.getElementById("data");
+const nome = prompt("Qual è il tuo nome?");
+console.log(nome);
 const lastName = prompt("Qual è il tuo cognome?");
 console.log(lastName);
 const favoriteColor = prompt("Qual è il tuo colore preferito?");
 console.log(favoriteColor);
-document
-  .getElementById("data")
-  .innerText("name + lastName + favoriteColor + 23");
+completeElement.innerText = nome + lastName + favoriteColor + "23";
+alert("la password generata per te è Boolean117");
